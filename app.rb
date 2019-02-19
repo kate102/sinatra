0d107ccba7dbb698c03f1660f1bfc17da6f4ea53
Student <student@Admins-MacBook-Pro-4.local>
2019-02-19 14:44:45 +0000

    Added erb functionality to give the cat a name

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
+get '/' do
+  'Hello Kate !'
+end
+
 get '/secret' do
   'Hello Message!'
 end
 
-require 'sinatra'
+# Views
+# Strip out the html and but in embedded ruby file
 get '/cat' do
   erb(:index)
 end
